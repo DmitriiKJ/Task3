57aa92f77069904520f50afc055863e44762e3cb
kurbatovdima <kurbatovdima2006@gmail.com>
2023-10-19 15:10:36 +0200

    Add project files.

---
+++ Project/Main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+
+// Створення таблиці констант
+char constTable[2][16] = {
+	// Для перших чотирьох бітів
+	{'9', '6', '3', '2', '8', 'B', '1', '7', 'A', '4', 'E', 'F', 'C', '0', 'D', '5'},
+	// Для останніх чотирьох бітів
+	{'3', '7', 'E', '9', '8', 'A', 'F', '0', '5', '2', '6', 'C', 'B', '4', 'D', '1'}
+};
+
+char newTable[2][16];
+
+char S_blok_1(char byte, char table[2][16]) // Шифрування
+{
+	char res;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 16; j++) 
+		{
+			if (i == 0 && j == byte >> 4)
+			{
+				res = (table[i][j] - '0') << 4;
+			}
+			else if (i == 1 && ((byte & 0x0F) == j))
+			{
+				res = res | (table[i][j] - '0');
+			}
+		}
+	}
+	return res;
+}
+
+void S_blok_2(const char table[2][16], char newTable[2][16]) 
+{
+	for (int i = 0; i < 2; i++) 
+	{
+		for (int j = 0; j < 16; j++) 
+		{
+			char value = table[i][j];
+			if (value == 'A' || value == 'B' || value == 'C' || value == 'D' || value == 'E' || value == 'F')
+			{
+				int index = value - '0' - 7;
+				if (j > 9)
+				{
+					newTable[i][index] = j + '0' + 7;
+				}
+				else
+				{
+					newTable[i][index] = j + '0';
+				}
+			}
+			else
+			{
+				int index = value - '0';
+				if (j > 9)
+				{
+					newTable[i][index] = j + '0' + 7;
+				}
+				else
+				{
+					newTable[i][index] = j + '0';
+				}
+			}
+		}
+	}
+}
+
+struct bites
+{
+	short bit1 : 1;
+	short bit2 : 1;
+	short bit3 : 1;
+	short bit4 : 1;
+	short bit5 : 1;
+	short bit6 : 1;
+	short bit7 : 1;
+	short bit8 : 1;
+};
+
+union char_bites
+{
+	char symbol;
+	bites bit;
+};
+
+char P_block_1(char_bites ch)
+{
+	char_bites res;
+	res.bit.bit1 = ch.bit.bit2;
+	res.bit.bit2 = ch.bit.bit8;
+	res.bit.bit3 = ch.bit.bit1;
+	res.bit.bit4 = ch.bit.bit5;
+	res.bit.bit5 = ch.bit.bit7;
+	res.bit.bit6 = ch.bit.bit3;
+	res.bit.bit7 = ch.bit.bit6;
+	res.bit.bit8 = ch.bit.bit4;
+	return res.symbol;
+}
+
+char P_block_2(char_bites ch)
+{
+	char_bites res;
+	res.bit.bit1 = ch.bit.bit3;
+	res.bit.bit2 = ch.bit.bit1;
+	res.bit.bit3 = ch.bit.bit6;
+	res.bit.bit4 = ch.bit.bit8;
+	res.bit.bit5 = ch.bit.bit4;
+	res.bit.bit6 = ch.bit.bit7;
+	res.bit.bit7 = ch.bit.bit5;
+	res.bit.bit8 = ch.bit.bit2;
+	return res.symbol;
+}
+
+int main()
+{
+	//------------------------ Шифрування S-блоку
+	char byte_after = 'p';
+	char byte_before = S_blok_1(byte_after, constTable);
+	cout << byte_after << " ---> " << byte_before << endl;
+
+	S_blok_2(constTable, newTable);
+
+	cout << byte_before << " ---> " << S_blok_1(byte_before, newTable) << endl;
+
+	//------------------------ Шифрування P-блоку
+	char_bites pb, res1, res2;
+	pb.symbol = 'c';
+	res1.symbol = P_block_1(pb);
+	cout << pb.symbol << " ----> " << res1.symbol << endl;
+	res2.symbol = P_block_2(res1);
+	cout << res1.symbol << " ----> " << res2.symbol << endl;
+	return 0;
+}
